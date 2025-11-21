d6d85c014e929f01ebf5f5db3b8601f7b2d2fcdc
Mario Aguiar <mario.aguiar@xwp.co>
2025-11-21 12:39:03 -0600

    Copy to clipboard

--- app/components/item.tsx
+++ app/components/item.tsx
@@ -1,11 +1,17 @@
+import { renderToString } from "react-dom/server";
 import { IconButton } from "@mui/material";
 import { Icon } from "@wordpress/icons";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Item( { icon, size, name }: { icon: any, size: number, name: string } ) {
+    const copyToClipboard = () => {
+        const stringIcon = renderToString( icon );
+        navigator.clipboard.writeText( stringIcon );
+    }
+    
     return (
         <div className="items-center flex flex-col gap-4 lg:w-52">
-            <IconButton>
+            <IconButton onClick={ copyToClipboard } >
                 <Icon icon={ icon } size={ size } />
             </IconButton>
 
