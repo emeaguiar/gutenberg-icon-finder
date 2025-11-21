b267cfa948a8a4af48e834dcd7fb04e11525faa1
Mario Aguiar <mario.aguiar@xwp.co>
2025-11-21 11:14:30 -0600

    Setup actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/gutenberg-icon-finder",
+  output: "export",
 };
 
 export default nextConfig;
