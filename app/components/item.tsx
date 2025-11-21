28a7104f36e201cb73755ff92a64951c99396d3f
Mario Aguiar <mario.aguiar@xwp.co>
2025-11-21 12:42:49 -0600

    Permitir copiar svg

--- app/components/item.tsx
+++ app/components/item.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { renderToString } from "react-dom/server";
-import { IconButton } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 import { Icon } from "@wordpress/icons";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Item( { icon, size, name }: { icon: any, size: number, name: string } ) {
-    const copyToClipboard = () => {
+    const [ copyText, setCopyText ] = useState<string>( "Copiar SVG al portapapeles" );
+
+    const copyToClipboard = async () => {
         const stringIcon = renderToString( icon );
-        navigator.clipboard.writeText( stringIcon );
+        await navigator.clipboard.writeText( stringIcon );
+
+        setCopyText( "¡Copiado!" );
+
+        setTimeout( () => {
+            setCopyText( "Copiar al portapapeles" );
+        }, 2000 );
     }
-    
+
     return (
         <div className="items-center flex flex-col gap-4 lg:w-52">
-            <IconButton onClick={ copyToClipboard } >
-                <Icon icon={ icon } size={ size } />
-            </IconButton>
+            <Tooltip title={ copyText } arrow placement="top">
+                <IconButton onClick={ copyToClipboard } >
+                    <Icon icon={ icon } size={ size } />
+                </IconButton>
+            </Tooltip>
 
             <code className="text-xs">{ name }</code>
         </div>
